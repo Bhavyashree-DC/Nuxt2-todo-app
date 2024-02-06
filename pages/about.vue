f9e7f0ed8931f20a551b79b0ca3ae4088d535c91
Bhavyashree_DC <bhavyadc05@gmail.com>
2024-02-06 13:58:10 +0530

    added TodoList Items

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+       <h2>About Page</h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolores deserunt mollitia quis voluptate. Deserunt, aspernatur.</p>
+       <NuxtLink to ='/'>Click Me</NuxtLink>
+    </div>
+ </template>
+ 
+ <script>
+ 
+ export default {
+   name: 'AboutPage',
+ }
+ </script>
+ 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
-  <Tutorial/>
+   <div class="container">
+    <div class="todo-app">
+      <div class="header">
+        <h1>{{ header }}</h1>
+      </div>
+      <TodoList></TodoList>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      header: 'Todo App',
+    };
+  },
+ 
 }
 </script>
+<style scoped>
+*{
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+
+.container {
+  width: 500px;
+  min-height: 900px;
+  background-color: #324d9e;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  margin: 20px;
+}
+
+.todo-app {
+  width: 430px;
+  max-height: 50px;
+  padding: 15px;
+  margin: 10px 15px 0px;
+}
+
+.todo-app .header {
+  text-align: center;
+  font-size: 1rem;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+</style>
