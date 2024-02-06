e3b7b084ec01371ea82fa23211f50a3f45b51472
Bhavyashree_DC <bhavyadc05@gmail.com>
2024-02-06 15:34:55 +0530

    added container at the center of the browser

--- pages/NewTaskForm.vue
+++ pages/NewTaskForm.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="add-new-task">
-        <div class="input-field">
-            <input v-model="newTask" placeholder="Enter task here..." />
-            <button @click="addTask"  @keyup.enter="addTask">Add</button>
+    <div class="add-container">
+        <div class="add-new-task">
+            {{ header }}
+            <div class="input-field">
+                <input v-model="newTask" placeholder="Enter task here..." />
+                <button @click="addTask"  @keyup.enter="addTask">Add</button>
+            </div>
         </div>
-    </div>
+    </div>   
   </template>
   
   <script>
@@ -41,45 +44,64 @@
   </script>
   
   <style scoped>
- 
+ .add-container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+ }
+
+ .add-new-task{
+    display: flex;
+    flex-direction: column;
+    gap:30px;
+    width:400px;
+    height: 500px;
+    font-size: 1.8rem;
+    font-weight: 600;
+    color: brown;
+    background-color: #fc9f9f;
+    border-radius: 20px;
+    gap: 20px;
+    text-align: center;
+    padding: 60px 30px;
+
+  
+ }
   .input-field {
-  display: flex;
-  /* align-items: center; */
-  justify-content: center;
-  background-color: #fc9f9f;
-  border-radius: 20px;
-  width: 400px;
-  height: 500px;
-  gap: 20px;
-  margin-top: 10px;
-  padding: 100px 20px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap:10px;
+    margin-top: 40px;
+
   }
   
-  .add-new-task input {
-  width: 300px;
-  height: 30px;
-  border: none;
-  outline: none;
-  background-color: #c7cbd6;
-  border-radius: 20px;
-  font-size: 18px;
-  padding: 18px;
-  color: #fff;
+  .input-field input {
+    width: 300px;
+    height: 30px;
+    border: none;
+    outline: none;
+    background-color: #c7cbd6;
+    border-radius: 20px;
+    font-size: 18px;
+    padding: 18px;
+    color: black;
   }
   
   .add-new-task button {
-  width: 80px;
-  height: 60px;
-  border-radius: 10px;
-  font-size: 20px;
-  cursor: pointer;
-  background-color: #FFCF96;
-  color: #fff;
-  border: none;
+    width: 80px;
+    height: 60px;
+    border-radius: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    background-color: #FFCF96;
+    color: #fff;
+    border: none;
   }
   
   .add-new-task button:hover {
-  background-color: coral;
+    background-color: coral;
   }
   </style>
   
